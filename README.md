791678d2b706848e34770ad8e23614892f732a3a
shanshan <1044917411@qq.com>
2017-12-12 13:00:18 +0800

    oppo

---
+++ README.md
@@ -0,0 +1,40 @@
+# oppo
+
+> 和闪一起写的oppo官网
+> *讲解知识点如下：*</br>
+> 1.vue动态添加class，使用:class="{className:isBoolean}",当isBoolean为true添加上此class，为false则remove此class。</br>
+> 2.vue动态添加style，:style="{borderTop:'1px solid '+ color(color为一个变量) }"</br>
+> 3.vue父到子传递数据，使用props。props可以使用数组接受，也可以使用对象。</br>
+``` bash
+   如<my-header :title='titles'/>
+   在myHeather内部接收title，可以:
+   props:{title:{
+    type: Array,
+    default: []
+   }}
+```
+>4.vue子到父传递数据，可以使用$emit。比如将<mySlide/>组件的当前index传递给父组件Home
+``` bash
+    我们可以在mySlide身上定义一个自定义事件，比如叫updateWhite;
+    <my-slide @updateWhite='handleUpdate'></my-slide>
+    在slide组件内部，我们可以用$emit来触发updateWhite事件。
+    在slide组件内：this.$emit('updateWhite',index)
+```
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Sections.vue
@@ -0,0 +1,184 @@
+<template>
+	<div class="section  container">
+		<div class="title">
+			<h2 class="text_h2">——发现话题——</h2>
+			<small>发现生活的美，找到属于自己的兴趣</small>
+		</div>
+		<ul class="list">
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+				<div class="mask">
+					<div class="info-wrap">
+						<i class="lt"></i>
+						<i class="rt"></i>
+						<i class="lb"></i>
+						<i class="rb"></i>
+					</div>
+					<div class="text-middle">
+						<h3 class="text_h3">周杰伦的2000W个故事</h3>
+						<p>5210人参与</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+			</li>
+			<li>
+				<a href=""><img src="../assets/img/1main.jpg" alt=""></a>
+			</li>
+		</ul>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+	</div>
+</template>
+<script>
+
+</script>
+<style lang='less' scoped>
+	.section {
+		margin-top: 100px;
+		text-align: center;
+		.title {
+			h2 {
+				color: #05b570;
+				font-size: 36px;
+				height: 36px;
+				margin-bottom: 20px;
+				font-weight: 400;
+			}
+			small {
+				display: block;
+				color: #666;
+				font-size: 16px;
+				margin-bottom: 20px;
+			}
+		}
+		.list {
+			width: 100%;
+			display: flex;
+			justify-content: center;
+
+			flex-wrap: wrap;
+			li {
+				flex: 1 1 30%; 
+				margin: 0.6em 1.2em;
+				position: relative;
+				a {
+					display: inline-block;
+					max-width: 100%;
+					img {
+						max-width: 100%;
+					}
+				}
+				.mask {
+					position: absolute;
+					transition: opacity ease-in .2s;
+					left: 0;
+					top: 0;
+					width: 100%;
+					height: 100%;
+					background: rgba(125, 125, 125, 0.7);
+					.info-wrap,
+					.text-middle {
+						position: absolute;
+						left: 50%;
+						top: 50%;
+						width: 74%;
+						height: 100%;
+						margin-left: -37%;
+						margin-top: -82px;
+						z-index: 1;
+						color: #fff;
+						text-align: center;
+						display: table;
+						i {
+							position: absolute;
+							width: 28px;
+							height: 23px;
+							border: 1px solid transparent;
+							opacity: .4;
+						}
+						.lt {
+							left: 0;
+							top: 0;
+							border-top-color: #fff;
+							border-left-color: #fff;
+						}
+						.rt {
+							right: 0;
+							top: 0;
+							border-top-color: #fff;
+							border-right-color: #fff;
+						}
+						.lb {
+							left: 0;
+							bottom: 0;
+							border-bottom-color: #fff;
+							border-left-color: #fff;
+						}
+						.rb {
+							right: 0;
+							bottom: 0;
+							border-bottom-color: #fff;
+							border-right-color: #fff;
+						}
+					}
+					.text-middle {
+						text-align: center;
+						h3 {
+							color: #fff;
+							width: 100%;
+							font-size: 2.2em;
+							font-weight: normal;
+							margin-top: 58px;
+						}
+						p {
+							width: 100%;
+							margin-top: 10px;
+							font-size: 1.6em;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@media screen and (max-width:767px) {
+		.container {
+      .text_h2{
+				font-size: 20px !important;
+				margin-bottom: 10px !important;
+			}
+			small{
+				font-size: 12px !important;
+			}
+			a {
+				max-width: 240px;
+			}
+			.text_h3{
+				font-size:12px!important;
+			}
+			p{
+			  font-size:8px!important;	
+			}
+		}
+	}
+</style>
